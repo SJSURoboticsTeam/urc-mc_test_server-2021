820f3164667073d25128a4cee41c6fbfe946ab77
naterpotatoers <nate.garza@hotmail.com>
2021-08-30 13:49:49 -0700

    removed ip endpoint & logic, will be replaced with VueJS soon

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const express = require("express");
-const path = require("path");
-const bent = require("bent");
 const os = require("os");
 
 const app = express().set("json spaces", 2);
 const port = 3000;
 const networkInterfaces = os.networkInterfaces();
-// console.log(networkInterfaces.wlp4s0);
+console.log(networkInterfaces);
 
 app.use(express.json({ limit: "2mb", extended: false }));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/views"));
-app.set("view engine", "ejs");
-
-let ip = "";
 
 var driveObject = {
   is_operational: 1,
-  drive_mode: "D",
+  drive_mode: "S",
   speed: 0,
   angle: 0,
 };
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("Home");
 });
 
 app.post("/ip", (req, res) => {
@@ -38,13 +32,11 @@ app.get("/drive", (req, res) => {
 });
 
 app.post("/drive", (req, res) => {
-  driveObject.is_operational = req.body.is_operational;
-  driveObject.drive_mode = req.body.drive_mode;
-  driveObject.speed = req.body.speed;
-  driveObject.angle = req.body.angle;
-  console.log(driveObject);
-  // res.json({ driveObject });
-  res.redirect("/");
+  driveObject.is_operational = req.body.is_operational + 0;
+  driveObject.drive_mode = req.body.drive_mode + "";
+  driveObject.speed = req.body.speed + 0;
+  driveObject.angle = req.body.angle + 0;
+  res.json({ driveObject });
 });
 
 app.post("/arm", (req, res) => {
@@ -54,6 +46,7 @@ app.post("/arm", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Computer:\thttp://localhost:3000`);
+  // Line below only works for Nate's T440p Thinkpad
   console.log(
     `Network:\thttp://${networkInterfaces.wlp4s0[0].address}:${port}`
   );
